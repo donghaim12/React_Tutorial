cd291d62461a2640fe2c4a3318d494f2a2a6e4be
donghaim12 <donghaim12@gmail.com>
2020-07-27 12:38:07 +0900

    Add Router Module

--- src/components/Appshell.js
+++ src/components/Appshell.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Drawer from '@material-ui/core/Drawer';
@@ -10,7 +12,6 @@ const styles = {
     root: {
         flexGrow: 1,
     },
-    //왼쪽에 메뉴버튼
     menuButton: {
         marginRight: 'auto'
     }
@@ -27,17 +28,34 @@ class AppShell extends React.Component {
     render() {
         const { classes } = this.props;
         return (
-            <div className={classes.root}>
-                <AppBar position="static">
-                    <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
-                        <MenuIcon/>
-                    </IconButton>
-                </AppBar>
-                <Drawer open={this.state.toggle}>
-                    <MenuItem onClick={this.handleDrawerToggle}>
-                        Home
-                    </MenuItem>
-                </Drawer>
+            <div>
+                <div className={classes.root}>
+                    <AppBar position="static">
+                        <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
+                            <MenuIcon/>
+                        </IconButton>
+                    </AppBar>
+                    <Drawer open={this.state.toggle}>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to="/">
+                                홈 화면
+                            </Link>
+                        </MenuItem>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to="/texts">
+                                텍스트 관리
+                            </Link>
+                        </MenuItem>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to="/words">
+                                단어 관리
+                            </Link>
+                        </MenuItem>
+                    </Drawer>
+                </div>
+                <div id="content" style={{margin: 'auto', marginTop: '20px'}}>
+                    {React.cloneElement(this.props.children)}
+                </div>
             </div>
         );
     }
